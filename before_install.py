194191bef399a30c5871e0f42bf48843d0b3e4dc
Matheus Degiovani <opensource@matheusd.com>
2017-12-28 17:22:19 -0200

    test

--- before_install.py
+++ before_install.py
@@ -12,10 +12,6 @@ def main():
     with open("versions.json") as f:
         versions = json.load(f)
 
-    print(os.environ)
-    os.makedirs("%(GOPATH)s/src/github.com/decred/dcrd" % os.environ)
-    system("git clone https://github.com/decred/dcrd %(GOPATH)s/src/github.com/decred/dcrd")
-    system("git clone https://github.com/decred/dcrwallet %(GOPATH)s/src/github.com/decred/dcrwallet")
     system("cd %(GOPATH)s/src/github.com/decred/dcrd && dep ensure && go build")
     system("cd %(GOPATH)s/src/github.com/decred/dcrwallet && dep ensure && go build")
 
