652a98c9de0dc03dae814f6486c1d851cc071098
Matheus Degiovani <opensource@matheusd.com>
2018-06-13 15:43:57 -0300

    Support custom repo in versions.json

--- before_install.py
+++ before_install.py
@@ -12,13 +12,16 @@ def main():
     with open("versions.json") as f:
         versions = json.load(f)
 
-    print("Checking out dcrd " + versions["shaDcrd"])
+    print("Checking out %s/dcrd/%s" % (versions["dcrdRepoOwner"], versions["shaDcrd"]))
+    system("git clone https://github.com/%s/dcrd %%(GOPATH)s/src/github.com/decred/dcrd" % versions["dcrdRepoOwner"])
     system("cd %(GOPATH)s/src/github.com/decred/dcrd && git checkout " + versions["shaDcrd"])
 
-    print("Checking out dcrwallet " + versions["shaDcrwallet"])
+    print("Checking out %s/dcrwallet/%s ", (versions["dcrwRepoOwner"], versions["shaDcrwallet"]))
+    system("git clone https://github.com/%s/dcrwallet %%(GOPATH)s/src/github.com/decred/dcrd" % versions["dcrwRepoOwner"])
     system("cd %(GOPATH)s/src/github.com/decred/dcrwallet && git checkout " + versions["shaDcrwallet"])
 
-    print("Checking out decrediton " + versions["shaDecrediton"])
+    print("Checking out %s/decrediton/%s ", (versions["decreditonRepoOwner"], versions["shaDecrediton"]))
+    system("git clone https://github.com/%s/decrediton %%(GOPATH)s/src/github.com/decred/decrediton" % versions["decreditonRepoOwner"])
     system("cd %(GOPATH)s/src/github.com/decred/decrediton && git checkout " + versions["shaDecrediton"])
 
     print("Fixing decrediton version")
